bf1eed5af31ad17509ef7ea8ae2ed6bb5a3f6d9b
Cgsanchez92626 <159073103+Cgsanchez92626@users.noreply.github.com>
2024-06-13 14:21:21 -0700

    Made changes for revision 3

--- index.js
+++ index.js
@@ -4,33 +4,52 @@
 // console.log(document.getElementById('add-todo'));
 
 const button = document.getElementById('add-todo');
-console.log(button);
+// console.log(button);
 
 
-//function to run when the button is clicked
-function handleAddTodo() {
-     
-    // create list item to go into my unordered list
-    const  ul = document.querySelector('ul');
-    console.log(ul);
-    const li = document.createElement('li');
-    // console.log('created list iem',li);
-
+// function to run when the button is clicked
+function handleAddTodo() {  
+    
     //access the input element and 
-    const input = document.querySelector('input');
+    const input = document.getElementById('todo-input');
+   // const input = document.querySelector('input');
    // Check if input value is empty
    if (input.value.trim() === '') {
     alert('Please enter a todo item.');
     return; // Stop execution if input is empty
    }
-    console.log(input.value);
-    // set the input text to my list item text
-    li.textContent = input.value;
+    // console.log(input.value);
+    // create list item to go into my unordered list
+    const ul = document.getElementById('todo-list');
+    const li = document.createElement('li');
+    // console.log(ul);
+    // console.log('created list iem',li);
+
+    // Create checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', function() {
+    // Toggle class for strikethrough effect when checkbox is checked
+       if (checkbox.checked) {
+           li.classList.add('completed');
+       } else {
+           li.classList.remove('completed');
+       }
+   });
+   // Set the input text to my list item text
+   const textSpan = document.createElement('span');
+   textSpan.textContent = input.value;
+    // Append checkbox and text span to the list item
+    li.appendChild(checkbox);
+    li.appendChild(textSpan);
+
     // append my list item to my ul (as a child element)
     ul.appendChild(li);
+    
     // console.log('button is clicked!')
     // Clear input field after adding todo
     input.value = '';
-};
+}
+ 
 //add an event listener
 button.addEventListener('click', handleAddTodo);
